629b1be1382bfd2480b7c4f9ad0bdd17669d9450
Yunitamaulida <55339552+Yunitamaulida@users.noreply.github.com>
2019-10-25 14:05:06 +0700

    Add files via upload

---
+++ js/pop_up_login.js
@@ -0,0 +1,12 @@
+var modal = document.getElementById("Pop_up_Login");
+		var btn = document.getElementById("Nav-Bar-Login");
+
+		var clospp = document.getElementById("closeppl");
+
+		btn.onclick = function(){
+			Pop_up_Login.style.display = "block";
+		}
+
+		clospp.onclick = function(){
+			Pop_up_Login.style.display = "none";
+		}
